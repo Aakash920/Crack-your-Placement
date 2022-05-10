2bee31600d09c190a6f2a8562e292e48025cf0e2
Aakash920 <84664685+Aakash920@users.noreply.github.com>
2022-05-10 11:58:10 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (84.68%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        //by Dutch National Flag Algorithm
+        int i=0, mid=0, j=nums.length-1;
+        while(mid<=j){
+            if(nums[mid]==0){
+                int temp=nums[mid];
+                nums[mid]=nums[i];
+                nums[i]=temp;
+                mid++;
+                i++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                int temp=nums[mid];
+                nums[mid]=nums[j];
+                nums[j]=temp;
+                j--;
+            }
+        }
+    }
+}
+
+
