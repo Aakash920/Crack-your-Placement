3a79e1004059079461f65a3f02e4eede99416d44
Aakash920 <84664685+Aakash920@users.noreply.github.com>
2022-05-20 11:58:37 +0530

    Time: 112 ms (87.22%), Space: 40.2 MB (94.64%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,55 +1,52 @@
 class Solution {
-    boolean visited[][];
     public boolean exist(char[][] b, String word) {
-//         int k=0;
-//         char cc=word.charAt(0);
-//         for(int i=0 ; i<b.length ; i++){
-//             for(int j=0 ; j<b[0].length ; j++){
-//                 if(k<word.length()){
-//                     cc=word.charAt(k);
-//                 }
-                
-//                 if(cc==b[i][j] && k<word.length()){
-//                     k++;
-//                 }else if(k==word.length()){
-//                     return true;
-//                 }
-//                 else{
-//                     if(i<b.length-1 && b[i+1][j]==cc){
-//                         k++;
-//                     }else if(j<b[0].length-1 && b[i][j+1]==cc){
-//                         k++;
-//                     }
-//                 }
-//             }
-//         }
-//         return false;
-        visited=new boolean[b.length][b[0].length];
         for(int i=0 ; i<b.length ; i++){
-            for(int j=0 ; j<b[0].length ; j++){
-                if(b[i][j]==word.charAt(0) && search(i,j,0,word,b)){
+            for(int j=0 ; j<b[i].length; j++){
+                if(backtrack(i,j,0,word,b)){
                     return true;
                 }
             }
         }
         return false;
     }
-    public boolean search(int i, int j, int k, String word, char[][] b){
-        if(k==word.length()) return true;
-        
-        if(i<0 || i==b.length || j<0 || j==b[0].length || word.charAt(k)!=b[i][j] || visited[i][j]){
-            return false;
-        }
-        
-        visited[i][j]=true;
-        if(search(i+1,j,k+1,word,b)||
-           search(i-1,j,k+1,word,b)||
-           search(i,j+1,k+1,word,b)||
-           search(i,j-1,k+1,word,b)){
+    private boolean backtrack(int i, int j, int k, String word, char[][] b){
+        if(k==word.length()){
             return true;
         }
-        visited[i][j]=false;
+        if(i<0 || j<0 || i>=b.length || j>=b[i].length || b[i][j]=='*'){
             return false;
+        }
+        if(b[i][j]!=word.charAt(k)){
+            return false;
+        }
+        b[i][j]='*';
+        boolean ans=(backtrack(i,j+1,k+1,word,b) ||
+                       backtrack(i+1,j,k+1,word,b) ||
+                       backtrack(i-1,j,k+1,word,b) ||
+                       backtrack(i,j-1,k+1,word,b));
+            
+        b[i][j]=word.charAt(k);
         
+        return ans;
     }
 }
+
+//  if(start == word.length())  return true;
+        
+//         if(r < 0 || c < 0 || r >= board.length || c >= board[r].length || board[r][c] == '*')
+//             return false;
+
+//         if(board[r][c] != word.charAt(start))   return false;
+
+//         // mark cell as visited
+//         board[r][c] = '*';
+        
+//         boolean result = (check(r,c+1,board,word,start+1) ||
+//                         check(r+1,c,board,word,start+1) ||
+//                         check(r-1,c,board,word,start+1) ||
+//                         check(r,c-1,board,word,start+1));
+        
+//         // unmark cell as not-visited
+//         board[r][c] = word.charAt(start);
+        
+//         return result;
