6c8f007c735b2cb3582f89220b04624a83ad2803
Aakash920 <84664685+Aakash920@users.noreply.github.com>
2022-05-27 13:23:44 +0530

    Time: 3 ms (37.16%), Space: 41.1 MB (55.34%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,19 +1,24 @@
 class Solution {
     public boolean isHappy(int n) {
-        if(n==1 || n==7 || n==1111111) return true;
-        while(n!=1){
-            if(n/10 == 0) return false;
-            int number=n;
-            int sum=0;
-            while(number!=0){
-                int num1=number%10;
-                int square=num1*num1;
-                sum+=square;
-                number/=10;
-            }
-            if(sum==1) return true;
-            n=sum;
+        HashSet<Integer> set=new HashSet<>();
+        return check(set,n);
+        
+    }
+    private boolean check(HashSet<Integer> set, int n){
+        if(n==1) return true;
+        if(set.contains(n)) return false;
+        
+        set.add(n);
+        n=update(n);
+        return check(set,n);
+    }
+    private int update(int n){
+        int updated=0;
+        while(n>0){
+            int digit=n%10;
+            updated+=(digit*digit);
+            n/=10;
         }
-        return false;
+        return updated;
     }
 }
