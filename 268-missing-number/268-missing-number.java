1c2ad73b51139f47e3e9c85c0a4a8fc8bf7ef1bf
Aakash920 <84664685+Aakash920@users.noreply.github.com>
2022-06-01 00:28:07 +0530

    Time: 1 ms (67.16%), Space: 51.1 MB (48.84%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,12 +1,26 @@
 class Solution {
     public int missingNumber(int[] arr) {
-        Arrays.sort(arr);
-        if(arr[0]!=0) return 0;
-        for(int i=1 ; i<arr.length ; i++){
-            if(arr[i]-arr[i-1]>1){
-                return arr[i-1]+1;
+        //O(nlog(n))
+        // Arrays.sort(arr);
+        // if(arr[0]!=0) return 0;
+        // for(int i=1 ; i<arr.length ; i++){
+        //     if(arr[i]-arr[i-1]>1){
+        //         return arr[i-1]+1;
+        //     }
+        // }
+        // return arr[arr.length-1]+1;
+        int[] nums=new int[arr.length+1];
+        for(int i=0 ; i<arr.length ; i++){
+            int idx=arr[i];
+            nums[idx]=1;
+        }
+        int ans=-1;
+        for(int i=0 ; i<nums.length ; i++){
+            if(nums[i]!=1){
+                ans=i;
+                break;
             }
         }
-        return arr[arr.length-1]+1;
+        return ans;
     }
 }
