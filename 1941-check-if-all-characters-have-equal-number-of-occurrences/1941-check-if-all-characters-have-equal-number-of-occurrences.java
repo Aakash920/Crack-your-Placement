c5ba57370038bea39b65a3832460091e936f79f6
Aakash920 <84664685+Aakash920@users.noreply.github.com>
2022-07-05 01:34:10 +0530

    Time: 12 ms (27.10%), Space: 42.6 MB (43.85%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.java
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.java
@@ -1,19 +1,40 @@
 class Solution {
     public boolean areOccurrencesEqual(String s) {
-        if(s.isEmpty()) return true;
-        if(s.equals("aca")) return false;
-        int[] arr=new int[26];
+//         if(s.isEmpty()) return true;
+        
+//         int[] arr=new int[26];
+//         ArrayList<Integer> list=new ArrayList<>();
+//         Arrays.fill(arr,-1);
+//         for(int i=0 ; i<s.length() ; i++){
+//             int idx=s.charAt(i)-'a';
+//             arr[idx]+=1;
+//         }
+//         for(int i=0 ; i<arr.length ; i++){
+//             if(arr[i]!=-1){
+//                 list.add(arr[i]);
+//             }
+//         }
+        
+//         for(int i=1 ; i<list.size(); i++){
+//             if(list.get(i)!=list.get(i-1)){
+//                 return false;
+//             }
+//         }
+//         return true;
+        
+        HashMap<Character,Integer> map=new HashMap<>();
         for(int i=0 ; i<s.length() ; i++){
-            int idx=s.charAt(i)-'a';
-            arr[idx]+=1;
+            char ch=s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        for(int i=0 ; i<arr.length ; i++){
-            if(arr[i]==0){
-                arr[i]=-1;
-            }
-        }
-        for(int i=1 ; i<arr.length ; i++){
-            if(arr[i]!=arr[i-1] && arr[i]!=-1 && arr[i-1]!=-1){
+        int first=map.get(s.charAt(0));
+        // for(Map.Entry<Character,Integer> e:map.entrySet()){
+        //     if(e.getValue()!=first){
+        //         return false;
+        //     }
+        // }
+        for(int i=1; i<s.length() ; i++){
+            if(map.get(s.charAt(i))!=first){
                 return false;
             }
         }
