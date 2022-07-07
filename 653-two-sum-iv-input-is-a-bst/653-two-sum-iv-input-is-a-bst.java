d496afa7773b3bc23fdce663537875d7ba86118b
Aakash Singh <84664685+Aakash920@users.noreply.github.com>
2022-07-07 17:01:40 +0530

    Time: 2 ms (99.30%), Space: 50.6 MB (71.37%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -18,43 +18,42 @@ class Solution {
         if(node==null){
             return false;
         }
-        // if(node.left==null && node.right==null && node.val!=k) {
-        //     return false;
-        // }
-        // return findTarget(node,node,k);
-        HashSet<Integer> set=new HashSet<>();
-        return targetSum(node, k, set);
-        
-    }
-    public boolean targetSum(TreeNode node, int k, HashSet<Integer> set){
-        if(node==null) return false;
-        if(set.contains(k-node.val)){
-            return true;
-        }else{
-            set.add(node.val);
+        if(node.left==null && node.right==null && node.val!=k) {
+            return false;
         }
-        return targetSum(node.left,k,set) || targetSum(node.right,k,set);
-    }
-//     public boolean findTarget(TreeNode root, TreeNode node, int k){
-//         if(node==null) return false;
-//         findTarget(root,node.left,k);
-//         int remaining=k-node.val;
-//         if(node.val<remaining){
-//             if(find(root,remaining)==true){
-//                 return true;
-//             }
-//         }
-//         findTarget(root,node.right,k);
-//     }
-//     private boolean find(TreeNode node,int target){
-//         if(node==null) return false;
+        return findTarget(node,node,k);
+//         HashSet<Integer> set=new HashSet<>();
+//         return targetSum(node, k, set);
         
-//         if(node.val>target){
-//             return find(node.left,target);
-//         }else if(node.val<target){
-//             return find(node.right,target);
-//         }else{
-//             return true;
-//         }
 //     }
+//     public boolean targetSum(TreeNode node, int k, HashSet<Integer> set){
+//         if(node==null) return false;
+//         if(set.contains(k-node.val)){
+//             return true;
+//         }else{
+//             set.add(node.val);
+//         }
+//         return targetSum(node.left,k,set) || targetSum(node.right,k,set);
+    }
+    public boolean findTarget(TreeNode root, TreeNode node, int k){
+        if(node==null) return false;
+        int remaining=k-node.val;
+        if(node.val<remaining){
+            if(find(root,remaining)==true){
+                return true;
+            }
+        }
+        return findTarget(root,node.left,k) || findTarget(root,node.right,k);
+    }
+    private boolean find(TreeNode node,int target){
+        if(node==null) return false;
+        
+        if(node.val>target){
+            return find(node.left,target);
+        }else if(node.val<target){
+            return find(node.right,target);
+        }else{
+            return true;
+        }
+    }
 }
