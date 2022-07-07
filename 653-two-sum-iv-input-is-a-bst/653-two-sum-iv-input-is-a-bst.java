13cc9c7bdb80771ae7175a4a3625f10ab1b9d39b
Aakash Singh <84664685+Aakash920@users.noreply.github.com>
2022-07-07 16:59:48 +0530

    Time: 7 ms (48.18%), Space: 50.9 MB (68.31%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean findTarget(TreeNode node, int k) {
+        if(node==null){
+            return false;
+        }
+        // if(node.left==null && node.right==null && node.val!=k) {
+        //     return false;
+        // }
+        // return findTarget(node,node,k);
+        HashSet<Integer> set=new HashSet<>();
+        return targetSum(node, k, set);
+        
+    }
+    public boolean targetSum(TreeNode node, int k, HashSet<Integer> set){
+        if(node==null) return false;
+        if(set.contains(k-node.val)){
+            return true;
+        }else{
+            set.add(node.val);
+        }
+        return targetSum(node.left,k,set) || targetSum(node.right,k,set);
+    }
+//     public boolean findTarget(TreeNode root, TreeNode node, int k){
+//         if(node==null) return false;
+//         findTarget(root,node.left,k);
+//         int remaining=k-node.val;
+//         if(node.val<remaining){
+//             if(find(root,remaining)==true){
+//                 return true;
+//             }
+//         }
+//         findTarget(root,node.right,k);
+//     }
+//     private boolean find(TreeNode node,int target){
+//         if(node==null) return false;
+        
+//         if(node.val>target){
+//             return find(node.left,target);
+//         }else if(node.val<target){
+//             return find(node.right,target);
+//         }else{
+//             return true;
+//         }
+//     }
+}
