0be6c62a0cb7cc6d28048f8a32ef9bbe680376ee
Aakash Singh <84664685+Aakash920@users.noreply.github.com>
2022-07-12 20:00:42 +0530

    Time: 1 ms (64.30%), Space: 42.9 MB (77.35%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -14,28 +14,36 @@
  * }
  */
 class Solution {
-    class Pair{
-        int dia=0;
-        int ht=-1;
-    }
+    // class Pair{
+    //     int dia=0;
+    //     int ht=-1;
+    // }
     public int diameterOfBinaryTree(TreeNode node) {
-        Pair mp=diameter(node);
-        return mp.dia;
+        int[] dia=new int[1];
+         find(node,dia);
+        return dia[0];
     }
-    public Pair diameter(TreeNode node){
-        if(node==null){
-            Pair bp=new Pair();
-            bp.dia=0;
-            bp.ht=-1;
-            return bp;
-        }
-        
-        Pair ls=diameter(node.left);
-        Pair rs=diameter(node.right);
-        
-        Pair mp=new Pair();
-        mp.ht=Math.max(ls.ht,rs.ht)+1;
-        mp.dia=Math.max(ls.ht+rs.ht+2,Math.max(ls.dia,rs.dia));
-        return mp;
+    public int find(TreeNode node, int[] dia){
+        if(node==null) return 0;
+        int ls=find(node.left,dia);
+        int rs=find(node.right,dia);
+        dia[0]=Math.max(dia[0],ls+rs);
+        return Math.max(ls,rs)+1;
     }
+//     public Pair diameter(TreeNode node){
+//         if(node==null){
+//             Pair bp=new Pair();
+//             bp.dia=0;
+//             bp.ht=-1;
+//             return bp;
+//         }
+        
+//         Pair ls=diameter(node.left);
+//         Pair rs=diameter(node.right);
+        
+//         Pair mp=new Pair();
+//         mp.ht=Math.max(ls.ht,rs.ht)+1;
+//         mp.dia=Math.max(ls.ht+rs.ht+2,Math.max(ls.dia,rs.dia));
+//         return mp;
+//     }
 }
