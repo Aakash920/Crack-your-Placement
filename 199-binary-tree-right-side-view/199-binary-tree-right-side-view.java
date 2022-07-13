3c94e2c8dc781e10898d9a97259860581a7e76df
Aakash Singh <84664685+Aakash920@users.noreply.github.com>
2022-07-14 01:04:17 +0530

    Time: 1 ms (94.70%), Space: 43.3 MB (5.45%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -15,26 +15,39 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        if(root==null){
-            List<Integer> list=new ArrayList<>();
-            return list;
-        }
-        List<Integer> list=new ArrayList<>();
-        LinkedList<TreeNode> q=new LinkedList<>();
-        q.addLast(root);
-        while(!q.isEmpty()){
-            int len=q.size();
-            TreeNode remove=null;
-            for(int i=0 ; i<len ; i++){
-                remove=q.removeFirst();
+//         if(root==null){
+//             List<Integer> list=new ArrayList<>();
+//             return list;
+//         }
+//         List<Integer> list=new ArrayList<>();
+//         LinkedList<TreeNode> q=new LinkedList<>();
+//         q.addLast(root);
+//         while(!q.isEmpty()){
+//             int len=q.size();
+//             TreeNode remove=null;
+//             for(int i=0 ; i<len ; i++){
+//                 remove=q.removeFirst();
                 
-                if(remove.left!=null)
-                    q.addLast(remove.left);
-                if(remove.right!=null)
-                    q.addLast(remove.right);
-            }
-            list.add(remove.val);
+//                 if(remove.left!=null)
+//                     q.addLast(remove.left);
+//                 if(remove.right!=null)
+//                     q.addLast(remove.right);
+//             }
+//             list.add(remove.val);
+//         }
+//         return list;
+        
+        
+        List<Integer> ans=new ArrayList<>();
+        rightView(root, ans, 0);
+        return ans;
+    }
+    public void rightView(TreeNode node, List<Integer> list, int level){
+        if(node==null) return;
+        if(level==list.size()){
+            list.add(node.val);
         }
-        return list;
+        rightView(node.right,list,level+1);
+        rightView(node.left,list,level+1);
     }
 }
