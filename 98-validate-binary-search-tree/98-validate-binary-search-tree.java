2946616ffe7bf27874ec3a48e5e9881ae47e5aab
Aakash Singh <84664685+Aakash920@users.noreply.github.com>
2022-07-17 03:21:30 +0530

    Time: 0 ms (100.00%), Space: 44.6 MB (13.84%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -15,17 +15,24 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode node) {
-        ArrayList<Integer> ans=new ArrayList<>();
-        inorder(node,ans);
-        for(int i=1 ; i<ans.size() ; i++){
-            if(ans.get(i)<=ans.get(i-1)) return false;
-        }
-        return true;
+        // ArrayList<Integer> ans=new ArrayList<>();
+        // inorder(node,ans);
+        // for(int i=1 ; i<ans.size() ; i++){
+        //     if(ans.get(i)<=ans.get(i-1)) return false;
+        // }
+        // return true;
+        return validate(node,null,null);
     }
-    public void inorder(TreeNode node,ArrayList<Integer> ans){
-        if(node==null) return ;
-        inorder(node.left,ans);
-        ans.add(node.val);
-        inorder(node.right,ans);
+    public boolean validate(TreeNode node,Integer max, Integer min){
+        if(node==null) return true;
+        if((max!=null && node.val>=max) || (min!=null && node.val<=min)) return false;
+        
+        return validate(node.left,node.val,min) && validate(node.right,max,node.val);
     }
+    // public void inorder(TreeNode node,ArrayList<Integer> ans){
+        // if(node==null) return ;
+        // inorder(node.left,ans);
+        // ans.add(node.val);
+        // inorder(node.right,ans);
+    // }
 }
