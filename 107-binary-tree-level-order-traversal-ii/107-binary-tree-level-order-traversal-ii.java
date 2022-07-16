e82cc5bdb671791a58e83809a0e0e886952f4056
Aakash Singh <84664685+Aakash920@users.noreply.github.com>
2022-07-17 02:20:39 +0530

    Time: 2 ms (58.50%), Space: 44 MB (12.44%) - LeetHub

--- 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.java
+++ 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.java
@@ -15,31 +15,53 @@
  */
 class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        //Solved from Level Order Traversal
         List<List<Integer>> ans=new ArrayList<>();
         
         if(root==null) return ans;
         LinkedList<TreeNode> q=new LinkedList<>();
         q.addLast(root);
         while(!q.isEmpty()){
-            List<Integer> list=new ArrayList<>();
+            ans.add(0,new ArrayList<>());
             int len=q.size();
             for(int i=0 ; i<len ; i++){
                 TreeNode node=q.removeFirst();
-                if(node!=null){
-                    list.add(node.val);
-                }
+                ans.get(0).add(node.val);
+                // if(node!=null){
+                //     list.add(node.val);
+                // }
                 if(node.left!=null)
                     q.addLast(node.left);
                 
                 if(node.right!=null)
                     q.addLast(node.right);
             }
-            if(list.size()>0){
-                ans.add(list);
-            }
+            // if(list.size()>0){
+            //     ans.add(list);
+            // }
             
         }
-        Collections.reverse(ans);
+        // Collections.reverse(ans);
         return ans;
+        
+        
+        // Solved From height concept
+        // int n=height(root);
+        // List<List<Integer>> ans=new ArrayList<>();
+        // inorder(ans,root,n);
+        // return ans;
     }
+    
+    // public void inorder(List<List<Integer>> list,TreeNode node, int n){
+    //     if(node==null) return;
+    //     inorder(list,node.left,n-1);
+    //     List<Integer> ans=new ArrayList<>();
+    //     ans.add(n-1,node.val);
+    //     list.add(ans);
+    //     inorder(list,node.right,n-1);
+    // }
+    // public int height(TreeNode node){
+    //     if(node==null) return 0;
+    //     return Math.max(height(node.left),height(node.right))+1;
+    // }
 }
