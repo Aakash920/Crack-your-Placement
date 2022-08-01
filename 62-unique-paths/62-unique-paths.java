772d41b23aa7bd227998fc2861392323233f5236
Aakash Singh <84664685+Aakash920@users.noreply.github.com>
2022-08-01 22:39:43 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (7.76%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -12,10 +12,27 @@ class Solution {
         // }
         // return dp[m-1][n-1];
         
-        long ans=1;
-        for(int i=m+n-2, j=1 ; i>=Math.max(m,n) ; i--, j++){
-            ans=(ans*i)/j;
+        // long ans=1;
+        // for(int i=m+n-2, j=1 ; i>=Math.max(m,n) ; i--, j++){
+        //     ans=(ans*i)/j;
+        // }
+        // return (int) ans;
+        
+        //By memorization
+        int[][] dp=new int[m+1][n+1];
+        return countWays(m,n,dp);
+        
+    }
+    public static int countWays(int m, int n, int[][] dp){
+        if(m==1 || n==1) return 1;
+        
+        if(dp[m][n]!=0){
+            return dp[m][n];
         }
-        return (int) ans;
+        int rows=countWays(m-1,n,dp);
+        int cols=countWays(m,n-1,dp);
+        int fn=rows+cols;
+        
+        return dp[m][n]=fn;
     }
 }
