99480bd3b9070a83b0ccd6c08ac2f725f064b063
Aakash Singh <84664685+Aakash920@users.noreply.github.com>
2022-09-15 12:26:13 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (99.86%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -1,16 +1,12 @@
 class Solution {
     public String longestCommonPrefix(String[] arr) {
-        if(arr.length==0) return "";
         String prefix=arr[0];
-        
-        for(int i=1 ; i<arr.length ; i++)
-            //get an index of prefix -> indexOf
-            //flow.indexOf(flower) means jo flow or flower equal nii h
-            while(arr[i].indexOf(prefix) != 0){
-                //removing last letter of prefix
+        for(int i=1 ; i<arr.length ; i++){
+            while(arr[i].indexOf(prefix)!=0){
                 prefix=prefix.substring(0,prefix.length()-1);
-                if(prefix.isEmpty()) return "";
+                if(prefix.length()==0) return "";
             }
+        }
         return prefix;
     }
 }
