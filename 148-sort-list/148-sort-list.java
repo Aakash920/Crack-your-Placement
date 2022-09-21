1e738e26185affbbdf6ead80909a6c6209b81962
Aakash Singh <84664685+Aakash920@users.noreply.github.com>
2022-09-21 19:17:33 +0530

    Time: 15 ms (79.59%), Space: 79.3 MB (34.72%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -10,27 +10,69 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if(head==null) return null;
-        if(head.next==null) return head;
-        ArrayList<Integer> list=new ArrayList<>();
-        ListNode curr=head;
-        while(curr!=null){
-            list.add(curr.val);
-            curr=curr.next;
+        // if(head==null) return null;
+        // if(head.next==null) return head;
+        // ArrayList<Integer> list=new ArrayList<>();
+        // ListNode curr=head;
+        // while(curr!=null){
+        //     list.add(curr.val);
+        //     curr=curr.next;
+        // }
+        // Collections.sort(list);
+        // ListNode temp=head;
+        // ListNode first=new ListNode(list.get(0));
+        // ListNode ans=first;
+        // first.next=null;
+        // int i=1;
+        // while(i<list.size() && temp!=null){
+        //     ListNode nn=new ListNode(list.get(i));
+        //     first.next=nn;
+        //     nn.next=null;
+        //     first=first.next;
+        //     i++;
+        // }
+        // return ans;
+        
+        //merge sort
+        if(head==null || head.next==null) return head;
+
+        ListNode slow=head;
+        ListNode fast=head;
+        ListNode prev=head;
+        while(fast!=null && fast.next!=null){
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        Collections.sort(list);
-        ListNode temp=head;
-        ListNode first=new ListNode(list.get(0));
-        ListNode ans=first;
-        first.next=null;
-        int i=1;
-        while(i<list.size() && temp!=null){
-            ListNode nn=new ListNode(list.get(i));
-            first.next=nn;
-            nn.next=null;
-            first=first.next;
-            i++;
+        prev.next=null;
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+
+        return merge(l1,l2);
+    }
+    public ListNode merge(ListNode l1,ListNode l2){
+        ListNode dummy=new ListNode(0);
+        ListNode temp=dummy;
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                temp.next=l1;
+                l1=l1.next;
+                temp=temp.next;
+            }else{
+                temp.next=l2;
+                l2=l2.next;
+                temp=temp.next;
+            }
         }
-        return ans;
+        if(l1!=null){
+            temp.next=l1;
+            l1=l1.next;
+        }
+        if(l2!=null){
+            temp.next=l2;
+            l2=l2.next;
+        }
+        return dummy.next;
+    
     }
 }
