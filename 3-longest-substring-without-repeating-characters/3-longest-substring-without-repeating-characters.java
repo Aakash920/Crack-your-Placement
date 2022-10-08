08458f50ab243ff883eeae8506354caa34b1046c
Aakash Singh <84664685+Aakash920@users.noreply.github.com>
2022-10-08 12:15:30 +0530

    Time: 3 ms (98.93%), Space: 42.3 MB (95.46%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -36,37 +36,53 @@ class Solution {
         // return ans;
         
         
-        HashMap<Character, Integer> map=new HashMap<>();
-        int i=-1, j=-1;
-        int ans=0;
-        while(true){
-            boolean f1=false;
-            boolean f2=false;
-            //aquire
-            while(i<s.length()-1){
-                f1=true;
-                i++;
-                char ch=s.charAt(i);
-                map.put(ch,map.getOrDefault(ch,0)+1);
+//         HashMap<Character, Integer> map=new HashMap<>();
+//         int i=-1, j=-1;
+//         int ans=0;
+//         while(true){
+//             boolean f1=false;
+//             boolean f2=false;
+//             //aquire
+//             while(i<s.length()-1){
+//                 f1=true;
+//                 i++;
+//                 char ch=s.charAt(i);
+//                 map.put(ch,map.getOrDefault(ch,0)+1);
                 
-                if(map.get(ch)==2){
-                    break;
-                }else{
-                    int len=i-j;
-                    ans=Math.max(ans,len);
-                }
+//                 if(map.get(ch)==2){
+//                     break;
+//                 }else{
+//                     int len=i-j;
+//                     ans=Math.max(ans,len);
+//                 }
+//             }
+//             //release
+//             while(j<i){
+//                 f2=true;
+//                 j++;
+//                 char ch=s.charAt(j);
+//                 map.put(ch,map.get(ch)-1);
+//                 if(map.get(ch)==1){
+//                     break;
+//                 } 
+//             }
+//             if(f1==false || f2==false) break;
+//         }
+//         return ans;
+        
+    
+        int[] fre=new int[256];
+        int l=0, r=0;
+        int ans=0;
+        while(r<s.length()){
+            fre[s.charAt(r)]++;
+            
+            while(fre[s.charAt(r)]>1){
+                fre[s.charAt(l)]--;
+                l++;
             }
-            //release
-            while(j<i){
-                f2=true;
-                j++;
-                char ch=s.charAt(j);
-                map.put(ch,map.get(ch)-1);
-                if(map.get(ch)==1){
-                    break;
-                } 
-            }
-            if(f1==false || f2==false) break;
+            ans=Math.max(r-l+1,ans);
+            r++;
         }
         return ans;
     }
