ae6d4443f26ab01922340cd2d67bb11264c591b7
Aakash Singh <84664685+Aakash920@users.noreply.github.com>
2023-01-31 13:16:29 +0530

    Time: 27 ms (65.25%), Space: 49.9 MB (60.89%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -2,19 +2,19 @@ class MyHashMap {
     int[] arr;
     public MyHashMap() {
         arr=new int[1000001];
-        Arrays.fill(arr,-1);
+        // Arrays.fill(arr,-1);
     }
     
     public void put(int key, int value) {
-        arr[key]=value;
+        arr[key]=value+1;
     }
     
     public int get(int key) {
-        return arr[key];
+        return arr[key]-1;
     }
     
     public void remove(int key) {
-        arr[key]=-1;
+        arr[key]=0;
     }
 }
 
