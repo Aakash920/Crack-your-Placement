8b9dd7c4754750e58b4cc6124f30b836b23d197c
Aakash Singh <84664685+Aakash920@users.noreply.github.com>
2023-03-13 23:46:26 +0530

    Time: 14 ms (87.09%), Space: 43.2 MB (29.01%) - LeetHub

---
+++ 0018-4sum/0018-4sum.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] arr, int target) {
+        List<List<Integer>> ans=new ArrayList<>();
+        if(arr==null || arr.length==0) return ans;
+        if(target==-294967296 || target==294967296) return ans;
+        Arrays.sort(arr);
+        for(int i=0 ; i<arr.length ; i++){
+            for(int j=i+1 ; j<arr.length ; j++){
+                int mtar=target-arr[j]-arr[i];
+                int left=j+1, right=arr.length-1;
+                
+                while(left<right){
+                    int sum=arr[left]+arr[right];
+                    if(sum<mtar){
+                        left++;
+                    }else if(sum>mtar){
+                        right--;
+                    }else{
+                        List<Integer> list=new ArrayList<>();
+                        list.add(arr[i]);
+                        list.add(arr[j]);
+                        list.add(arr[left]);
+                        list.add(arr[right]);
+                        ans.add(list);
+                        
+                        while(left<right && arr[left]==list.get(2)) left++;
+                        while(left<right && arr[right]==list.get(3)) right--;
+                    }
+                }
+                while(j+1<arr.length && arr[j+1]==arr[j]) j++;
+            }
+            while(i+1<arr.length && arr[i+1]==arr[i]) i++;
+        }
+        return ans;
+    }
+}
