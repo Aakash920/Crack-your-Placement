a8c2406896aa796b50024fc49a591fc4e0ab5546
Aakash Singh <84664685+Aakash920@users.noreply.github.com>
2023-03-22 22:06:19 +0530

    Time: 1 ms (81.82%), Space: 40.4 MB (90.78%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -3,10 +3,7 @@ class Solution {
         StringBuilder sb=new StringBuilder();
         int i=0, j=0;
         while(i<s1.length() && j<s2.length()){
-            sb.append(s1.charAt(i));
-            sb.append(s2.charAt(j));
-            i++;
-            j++;
+            sb.append(s1.charAt(i++)).append(s2.charAt(j++));
         }
         while(i<s1.length()) sb.append(s1.charAt(i++));
         while(j<s2.length()) sb.append(s2.charAt(j++));
