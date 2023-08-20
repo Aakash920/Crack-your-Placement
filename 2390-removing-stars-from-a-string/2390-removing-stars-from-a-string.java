b5739dbdffcb83fb4ab35faa62194d581807a3d2
Aakash Singh <84664685+Aakash920@users.noreply.github.com>
2023-08-20 13:59:07 +0530

    Time: 34 ms (88.38%), Space: 44.6 MB (87.88%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,17 +1,26 @@
 class Solution {
     public String removeStars(String s) {
-        StringBuilder sb=new StringBuilder(s);
-        if(sb.charAt(0)=='*') sb.deleteCharAt(0);
-        int i=1;
-        while(i<sb.length()){
-            if(sb.charAt(i)=='*'){
-                sb.deleteCharAt(i);
-                sb.deleteCharAt(i-1);
-                i-=2;
+        // StringBuilder sb=new StringBuilder(s);
+        // if(sb.charAt(0)=='*') sb.deleteCharAt(0);
+        // int i=1;
+        // while(i<sb.length()){
+        //     if(sb.charAt(i)=='*'){
+        //         sb.deleteCharAt(i);
+        //         sb.deleteCharAt(i-1);
+        //         i-=2;
+        //     }
+        //     i++;
+        // }
+        // return sb.toString();
+        
+        StringBuilder sb=new StringBuilder();
+        for(int i=0 ; i<s.length() ; i++){
+            if(s.charAt(i)=='*'){
+                sb.deleteCharAt(sb.length()-1);
+            }else{
+                sb.append(s.charAt(i));
             }
-            i++;
         }
         return sb.toString();
-        
     }
 }
