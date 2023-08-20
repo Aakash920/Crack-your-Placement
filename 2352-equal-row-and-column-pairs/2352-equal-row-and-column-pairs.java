9ba13c78e2ca8f4418d25f6795a915db0333ed88
Aakash Singh <84664685+Aakash920@users.noreply.github.com>
2023-08-20 17:45:26 +0530

    Time: 44 ms (56.59%), Space: 50.8 MB (37.58%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,18 +1,35 @@
 class Solution {
     public int equalPairs(int[][] arr) {
+        // int count=0;
+        // int n=arr.length;
+        // for(int i=0 ; i<arr.length ; i++){
+        //     for(int j=0 ; j<arr.length ; j++){
+        //         int k=0;
+        //         for( ; k<arr.length ; k++){
+        //             if(arr[i][k]!=arr[k][j]){
+        //                 break;
+        //             }
+        //         }
+        //         if(k==n){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+        
         int count=0;
-        int n=arr.length;
+        HashMap<String,Integer> map=new HashMap<>();
+        for(int[] grid:arr){
+            String s=Arrays.toString(grid);
+            map.put(s,map.getOrDefault(s,0)+1);
+        }
         for(int i=0 ; i<arr.length ; i++){
+            int[] col=new int[arr.length];
             for(int j=0 ; j<arr.length ; j++){
-                int k=0;
-                for( ; k<arr.length ; k++){
-                    if(arr[i][k]!=arr[k][j]){
-                        break;
-                    }
-                }
-                if(k==n){
-                    count++;
-                }
+                col[j]=arr[j][i];
+            }
+            if(map.containsKey(Arrays.toString(col))){
+                count+=map.get(Arrays.toString(col));
             }
         }
         return count;
